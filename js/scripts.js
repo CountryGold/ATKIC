75d4080a5031673e85d345f1185aeb3d43b803c0
CountryGold <country.gold@gmail.com>
2016-05-03 17:16:17 +0900

    try
    try index location

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Empty JS for your own code to be here
+function goPDT()
+    {
+        document.getElementById("content1").src="PDT/product.html";
+    }
+function goCNT()
+    {
+        document.getElementById("content1").src="CNT/contact.html";
+    }
+function goATK()
+    {
+        document.getElementById("content1").src="atkic.html";
+    }
+function goAPP()
+    {
+        document.getElementById("content1").src="APP/application.html";
+    }
